01232280707876876da8602af67be7db442d512f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-15 14:03:17 +0200

    Create circ.java

---
+++ circ.java
@@ -0,0 +1,9 @@
+import java.util.Vector;
+import java.awt.*;
+
+class Ejemplo1 {
+  public static void main (String arg[]) throws InterruptedException
+  {
+    System.out.println("Comienza main()...");
+  }
+}
